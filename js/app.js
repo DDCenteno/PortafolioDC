c8c0578203c9e43a056ed05bd75f2071212052d6
DDCenteno <danielacentenoramos@gmail.com>
2018-02-16 04:40:29 -0500

    mejorando

---
+++ js/app.js
@@ -0,0 +1,18 @@
+
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+
+    $(window).on('scroll', function() {
+        if ($(window).scrollTop()) {
+          $('#menu').addClass('blackback');
+          $('#Logo').addClass('DCLogoHover');
+        } else {
+          $('#menu').removeClass('blackback');
+          $('#Logo').removeClass('DCLogoHover');
+        }
+      });
+
+      $('#title').addClass('animated slideInDown');
+});
+
